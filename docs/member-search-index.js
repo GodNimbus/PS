ed40022f81ba6992efeb296fb055632ba3424d46
sara21gg <dawbsgarrigagomila@iesjoanramis.org>
2021-11-25 17:42:46 +0100

    primer archivo java con documentacion

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"sara","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"sara","l":"sara()","url":"%3Cinit%3E()"}]
---
+++ sara.java
@@ -0,0 +1,8 @@
+/**
+documentacion for the new class Sara, java documentation exercice
+*/
+public class sara {
+	public static void main (String args[]){
+		System.out.println("Hola Sara");
+	}
+}
